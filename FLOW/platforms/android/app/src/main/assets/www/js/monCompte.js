53eb3201b1d2bbe2a0a1032c1d8ce507ecdf3b33
ippa16 <dacostaalexis95@gmail.com>
2019-02-26 10:35:21 +0100

    photo et barre follow
    avancement sur mon compte
    bar du haut et photo de profile

---
+++ FLOW/platforms/android/app/src/main/assets/www/js/monCompte.js
@@ -0,0 +1,13 @@
+var nameMonCompte = "Vanessa";
+var mainView = app.addView('.view-main');
+ 
+app.onPageInit('login-screen', function (page) {
+    $(".fflow-btn").css("display", "none");
+    $("#fprofilPicture").css({"background-image":"url('src/pictures/girl1.jpg')"});
+    $("#fnameMonCompte").append(nameMonCompte);
+    $("#fgobackmonCompte").click(function(){
+        mainView.back();
+        $(".fflow-btn").css("display", "block");
+    });
+}); 
+
