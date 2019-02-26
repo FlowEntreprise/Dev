4cde188f6efb5b0575ef38bde14a44c507a4e4c8
ippa16 <dacostaalexis95@gmail.com>
2019-02-26 11:41:50 +0100

    save

--- FLOW/platforms/browser/www/js/monCompte.js
+++ FLOW/platforms/browser/www/js/monCompte.js
@@ -1,10 +1,11 @@
 var nameMonCompte = "Vanessa";
+var bioMonCompte ="Hey, I'm using Flow";
 var mainView = app.addView('.view-main');
  
 app.onPageInit('login-screen', function (page) {
     $(".fflow-btn").css("display", "none");
-    $("#fprofilPicture").css({"background-image":"url('../src/pictures/girl2.jpg')"});
-    $("#fnameMonCompte").append(nameMonCompte);
+    $("#fprofilPicture").css({"background-image":"url('src/pictures/girl1.jpg')"});
+    $("#fMonCompteSpace").append(nameMonCompte);
     $("#fgobackmonCompte").click(function(){
         mainView.back();
         $(".fflow-btn").css("display", "block");
