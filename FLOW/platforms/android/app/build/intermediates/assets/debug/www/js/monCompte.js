ad2fc65d7e89c6f40494c93e0fc01667de3a7bdf
ippa16 <dacostaalexis95@gmail.com>
2019-03-04 17:18:06 +0100

    onglet mon compte terminé

--- FLOW/platforms/android/app/build/intermediates/assets/debug/www/js/monCompte.js
+++ FLOW/platforms/android/app/build/intermediates/assets/debug/www/js/monCompte.js
@@ -1,13 +1,36 @@
 var nameMonCompte = "Vanessa";
+var bioMonCompte ="Hey, I'm using Flow";
 var mainView = app.addView('.view-main');
+var FlowBandeau = 12;
+var Follower = 305;
+var Following = 250;
  
 app.onPageInit('login-screen', function (page) {
     $(".fflow-btn").css("display", "none");
     $("#fprofilPicture").css({"background-image":"url('src/pictures/girl1.jpg')"});
-    $("#fnameMonCompte").append(nameMonCompte);
+    $("#fMonCompteSpace").append(nameMonCompte);
+    $("#ffLowBandeauChiffre").append(FlowBandeau);
+    $("#ffollowersBandeauChiffre").append(Follower);
+    $("#ffollowingBandeauChiffre").append(Following);
+    $("#fbioMonCompte").append(bioMonCompte);
+
     $("#fgobackmonCompte").click(function(){
         mainView.back();
         $(".fflow-btn").css("display", "block");
     });
+
+    // $("#fLikes").click(function(){
+    //     $("#fLikes").css("color", "#1A84EF");
+    //     $("#fMyActivity").css("color", "#92ABB2");
+    //     $("#funderlineMyActivity").css("display", "none");
+    //     $("#funderlineLike").css("display", "block");
+    // });
+
+    // $("#fMyActivity").click(function(){
+    //     $("#fMyActivity").css("color", "#1A84EF");
+    //     $("#fLikes").css("color", "#92ABB2");
+    //     $("#funderlineMyActivity").css("display", "block");
+    //     $("#funderlineLike").css("display", "none");
+    // });
 }); 
 
