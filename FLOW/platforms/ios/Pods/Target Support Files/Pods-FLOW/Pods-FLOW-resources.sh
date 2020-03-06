8665d22fac7e002b96005f42cfbd36960656b57d
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2020-03-06 03:44:12 -0800

    merge

--- FLOW/platforms/ios/Pods/Target Support Files/Pods-FLOW/Pods-FLOW-resources.sh
+++ FLOW/platforms/ios/Pods/Target Support Files/Pods-FLOW/Pods-FLOW-resources.sh
@@ -16,7 +16,7 @@ fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 
-RESOURCES_TO_COPY=${SRCROOT}/Pods/resources-to-copy-${TARGETNAME}.txt
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
 > "$RESOURCES_TO_COPY"
 
 XCASSET_FILES=()
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${SRCROOT}/Pods/FBSDKCoreKit/FacebookSDKStrings.bundle"
+  install_resource "${PODS_ROOT}/FBSDKCoreKit/FacebookSDKStrings.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${SRCROOT}/Pods/FBSDKCoreKit/FacebookSDKStrings.bundle"
+  install_resource "${PODS_ROOT}/FBSDKCoreKit/FacebookSDKStrings.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
