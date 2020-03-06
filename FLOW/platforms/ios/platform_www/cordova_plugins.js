308d3387a7747da767bad2aace34290b01ca7466
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2020-03-06 16:35:06 +0100

    record audio on ios

--- FLOW/platforms/ios/platform_www/cordova_plugins.js
+++ FLOW/platforms/ios/platform_www/cordova_plugins.js
@@ -229,6 +229,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
         "cordova.plugins.backgroundMode",
         "plugin.backgroundMode"
       ]
+    },
+    {
+      "id": "cordova-plugin-audioinput.AudioInput",
+      "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+      "pluginId": "cordova-plugin-audioinput",
+      "clobbers": [
+        "audioinput"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -238,6 +246,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "twitter-connect-plugin": "0.6.0",
     "cordova-plugin-facebook4": "6.3.0",
     "cordova-plugin-device": "2.0.3",
-    "cordova-plugin-background-mode": "0.7.3"
+    "cordova-plugin-background-mode": "0.7.3",
+    "cordova-plugin-audioinput": "1.0.2"
   };
 });
