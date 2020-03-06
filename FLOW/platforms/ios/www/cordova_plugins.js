055a527aa87d9282cc8f29bebf0b1aceffc78e2c
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2020-03-06 15:09:01 +0100

    plugin audio

--- FLOW/platforms/ios/www/cordova_plugins.js
+++ FLOW/platforms/ios/www/cordova_plugins.js
@@ -212,6 +212,23 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "facebookConnectPlugin"
       ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-background-mode.BackgroundMode",
+      "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
+      "pluginId": "cordova-plugin-background-mode",
+      "clobbers": [
+        "cordova.plugins.backgroundMode",
+        "plugin.backgroundMode"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -219,6 +236,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-file": "6.0.2",
     "cordova-plugin-media": "5.0.3",
     "twitter-connect-plugin": "0.6.0",
-    "cordova-plugin-facebook4": "6.3.0"
+    "cordova-plugin-facebook4": "6.3.0",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-background-mode": "0.7.3"
   };
 });
