08d1fff986a1a329d643049da45a82eaf495ece2
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2020-04-10 02:35:48 +0200

    merged from ios

--- FLOW/www/js/index.js
+++ FLOW/www/js/index.js
@@ -35,7 +35,6 @@ var app = {
             StatusBar.backgroundColorByHexString("#f7f7f8");
             let custom_vh = window.innerHeight / 100;
             document.documentElement.style.setProperty("--custom-vh", custom_vh + "px");
-
         }, 500);
 
         this.receivedEvent('deviceready');
@@ -175,15 +174,16 @@ var app = {
                 if (window.cordova.platformId == "ios") {
                     data.additionalData.sender_info = JSON.parse(data.additionalData.sender_info);
                 }
-                if (data.additionalData.type == "story_comment") { return; }
+                if (data.additionalData.type == "story_comment") {
+                    return;
+                }
                 if (data.additionalData.type == "follow") {
                     let data_go_to_account = {
                         private_Id: data.additionalData.sender_info.privateId,
                         user_private_Id: window.localStorage.getItem("user_private_id")
                     };
                     go_to_account(data_go_to_account);
-                }
-                else {
+                } else {
 
                     $(".flow_specifique_container").html("");
                     let myApp = new Framework7();
@@ -285,6 +285,7 @@ function online() {
     console.log("you are online");
     ServerManager.GetStory();
 }
+
 
 
 /*
