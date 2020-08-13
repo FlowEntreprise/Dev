48f25c401a7f20cacd0b7cb1cf8bcc34af7efd87
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2020-08-13 21:07:54 +0200

    Updated Gradle to 6.1.1 and fixed vscode building

--- FLOW/www/js/story.js
+++ FLOW/www/js/story.js
@@ -51,11 +51,11 @@ const pSBC = (p, c0, c1, l) => {
         b: 0,
         a: -1
     } : {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: -1
-        }, p = P ? p * -1 : p, P = 1 - p;
+        r: 255,
+        g: 255,
+        b: 255,
+        a: -1
+    }, p = P ? p * -1 : p, P = 1 - p;
     if (!f || !t) return null;
     if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
     else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
@@ -914,7 +914,7 @@ function loadStoryComments(data) {
         comment_pp.className = "fstory_comment_pp";
         comment_pp.style.backgroundImage = "url(" + com.user_picture + ")";
         let comment_play = document.createElement("div");
-        comment_play.setAttribute("comment_id", i);
+        comment_play.setAttribute("comment_id", (i + story_comment_index * 10));
         comment_play.className = "fstory_comment_play";
         comment_play.onclick = function () {
             if (com.ready) {
