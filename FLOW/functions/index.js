4e1fdc0f063b8cc758366f6c94084160138f62ff
chris <32330578+dahed92@users.noreply.github.com>
2021-01-26 20:47:52 +0100

    DevCloudFunctions

--- FLOW/functions/index.js
+++ FLOW/functions/index.js
@@ -65,103 +65,103 @@ function promisifyCommand(command) {
 
 // CE CODE FONCTIONNE PARFAITEMENT
 
-exports.EncodeMp3 = functions.storage.object().onFinalize(async (object) => {
+exports.DevEncodeMp3 = functions.storage.object().onFinalize(async (object) => {
   const fileBucket = object.bucket;
   const filePath = object.name;
   const contentType = object.contentType;
   const fileName = path.basename(filePath);
-  console.log(`le senderId est : ${object.metadata.senderId}`);
 
-  if (!contentType.startsWith('audio/')) {
-    console.log('This is not an audio.');
+  if (filePath.startsWith('dev/')) {
+
+    if (contentType.startsWith('audio/')) {
+
+      if (!fileName.endsWith('.mp3')) {
+
+        const bucket = gcs.bucket(fileBucket);
+        const tempFilePath = path.join(os.tmpdir(), fileName);
+
+        const targetTempFileName = fileName.replace(/\.[^/.]+$/, '') + object.metadata.senderId + '.mp3';
+        const targetTempFilePath = path.join(os.tmpdir(), targetTempFileName);
+        const targetStorageFilePath = path.join(path.dirname(filePath), targetTempFileName);
+
+        await bucket.file(filePath).download({ destination: tempFilePath });
+        console.log('Audio downloaded locally to', tempFilePath);
+
+        const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+        const ffmpeg = require('fluent-ffmpeg');
+        let command = ffmpeg(tempFilePath)
+          .setFfmpegPath(ffmpegPath)
+          .toFormat('mp3')
+
+          .save(targetTempFilePath);
+
+
+        await promisifyCommand(command);
+        console.log('Output audio created at', targetTempFilePath);
+
+
+
+        console.log("delete wave file");
+        await bucket.file(filePath).delete();
+        // Uploading the audio.
+        await bucket.upload(targetTempFilePath, { destination: targetStorageFilePath }).then(stuff => {
+          console.log('Audio saved successfully.');
+          //bucket.upload(targetTempFilePath);
+          let file = bucket.file(targetStorageFilePath);
+          return file.getSignedUrl({
+            action: 'read',
+            expires: '03-09-2500'
+          });
+        }).then(urls => {
+          var url = urls[0];
+          console.log(`Audio url = ${url}`);
+          // UPDATE REALTIME DATABASE ICI ET NE PAS OUBLIER DE SET LE SEEN
+          let dataMessage = {
+            "senderId": object.metadata.senderId, //sender firebase token
+            "memberId": object.metadata.memberId, //reciever firebase token
+            "memberLastOs": object.metadata.memberLastOs,
+            "memberRegistrationId": object.metadata.memberRegistrationId,
+            "memberprofilePic": object.metadata.memberprofilePic,
+            "senderPrivateId": object.metadata.senderPrivateId,
+            "senderFullName": object.metadata.senderFullName,
+            "chatId": object.metadata.chatId,
+            "message": object.metadata.message,
+            "Environnement": object.metadata.Environnement,
+            "seen_by": {
+              [object.metadata.senderId]: true
+            },
+            "image": object.metadata.image,
+            "audio": url,
+            "time": Date.now()
+          };
+          return AddMessageToFirebase(dataMessage);
+        }).catch(err => {
+          console.log(`Unable to upload audio ${err.message}`);
+        });
+
+        fs.unlinkSync(tempFilePath);
+        fs.unlinkSync(targetTempFilePath);
+        return null;
+      }
+      return null;
+    }
     return null;
   }
-
-
-  if (fileName.endsWith('.mp3')) {
-    console.log('Already a converted audio.');
-    return null;
-  }
-
-
-  const bucket = gcs.bucket(fileBucket);
-  const tempFilePath = path.join(os.tmpdir(), fileName);
-
-  const targetTempFileName = fileName.replace(/\.[^/.]+$/, '') + object.metadata.senderId + '.mp3';
-  const targetTempFilePath = path.join(os.tmpdir(), targetTempFileName);
-  const targetStorageFilePath = path.join(path.dirname(filePath), targetTempFileName);
-
-  await bucket.file(filePath).download({ destination: tempFilePath });
-  console.log('Audio downloaded locally to', tempFilePath);
-
-  const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-  const ffmpeg = require('fluent-ffmpeg');
-  let command = ffmpeg(tempFilePath)
-    .setFfmpegPath(ffmpegPath)
-    .toFormat('mp3')
-
-    .save(targetTempFilePath);
-
-
-  await promisifyCommand(command);
-  console.log('Output audio created at', targetTempFilePath);
-
-
-
-  console.log("delete wave file");
-  await bucket.file(filePath).delete();
-  // Uploading the audio.
-  await bucket.upload(targetTempFilePath, { destination: targetStorageFilePath }).then(stuff => {
-    console.log('Audio saved successfully.');
-    //bucket.upload(targetTempFilePath);
-    let file = bucket.file(targetStorageFilePath);
-    return file.getSignedUrl({
-      action: 'read',
-      expires: '03-09-2500'
-    });
-  }).then(urls => {
-    var url = urls[0];
-    console.log(`Audio url = ${url}`);
-    // UPDATE REALTIME DATABASE ICI ET NE PAS OUBLIER DE SET LE SEEN
-    let dataMessage = {
-      "senderId": object.metadata.senderId, //sender firebase token
-      "memberId": object.metadata.memberId, //reciever firebase token
-      "memberLastOs": object.metadata.memberLastOs,
-      "memberRegistrationId": object.metadata.memberRegistrationId,
-      "memberprofilePic": object.metadata.memberprofilePic,
-      "senderPrivateId": object.metadata.senderPrivateId,
-      "senderFullName": object.metadata.senderFullName,
-      "chatId": object.metadata.chatId,
-      "message": object.metadata.message,
-      "Environnement": object.metadata.Environnement,
-      "seen_by": {
-        [object.metadata.senderId]: true
-      },
-      "image": object.metadata.image,
-      "audio": url,
-      "time": Date.now()
-    };
-    return AddMessageToFirebase(dataMessage);
-  }).catch(err => {
-    console.log(`Unable to upload audio ${err.message}`);
-  });
-
-  fs.unlinkSync(tempFilePath);
-  fs.unlinkSync(targetTempFilePath);
   return null;
-
-  // Once the audio has been uploaded delete the local file to free up disk space.
 
 });
 
 
 function AddMessageToFirebase(dataMessage) {
-  let ref = admin.database().ref(dataMessage.Environnement + '/messages/' + dataMessage.chatId);
-  ref.push().set(dataMessage).then(() => {
-    return GetMessageId(dataMessage);
-  }).catch(err => {
-    console.log(`Unable to Add message to Firebase ${err.message}`);
-  });
+  if (dataMessage.Environnement === "dev") {
+    let ref = admin.database().ref(dataMessage.Environnement + '/messages/' + dataMessage.chatId);
+    ref.push().set(dataMessage).then(() => {
+      return GetMessageId(dataMessage);
+    }).catch(err => {
+      console.log(`Unable to Add message to Firebase ${err.message}`);
+    });
+  }
+
 }
 
 function GetMessageId(dataMessage) {
