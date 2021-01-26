d35f10c9ab14b4a6f93adbabc5115e5c1a5e1f5e
chris <32330578+dahed92@users.noreply.github.com>
2021-01-26 21:11:56 +0100

    Cloud Functions
    - Creation d'une version de dev et de prod de la fonction EncodeMP3

--- FLOW/functions/index.js
+++ FLOW/functions/index.js
@@ -20,50 +20,109 @@ function promisifyCommand(command) {
   });
 }
 
-/**
- * When an audio is uploaded in the Storage bucket We generate a mono channel audio automatically using
- * node-fluent-ffmpeg.
- */
+// Deploy all functions : firebase deploy --only functions
+// Deploy specific function : firebase deploy --only functions:functionnName
 
-/*exports.UploadVoiceNote = functions.https.onCall(async (data, context) => {
+/*--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD -
+  --PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD -
+  --PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD--PROD - */
 
-  const uuidv1 = require('uuid/v1');
-  console.log('Saving audio to GCS.');
-  var bucket = admin.storage().bucket('flow-85249.appspot.com');
-  var uuid = uuidv1();
-  let before_encode = `${data.time}.wav`;
-  let after_encode = `${data.time}${data.senderId}.mp3`;
-  const tempFilePath = path.join(os.tmpdir(), before_encode);
-  const targetTempFilePath = path.join(os.tmpdir(), after_encode);
-  const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-  const ffmpeg = require('fluent-ffmpeg');
+exports.ProdEncodeMp3 = functions.storage.object().onFinalize(async (object) => {
+  const fileBucket = object.bucket;
+  const filePath = object.name;
+  const contentType = object.contentType;
+  const fileName = path.basename(filePath);
 
-  var command = ffmpeg(data.sound)
-    .setFfmpegPath(ffmpegPath)
-    .toFormat('mp3')
-    .save(targetTempFilePath);
+  if (filePath.startsWith('prod/')) {
 
-  await promisifyCommand(command);
-  await bucket.upload(targetTempFilePath, { destination: after_encode }).then(stuff => {
-    console.log("MP3 succesfully uploaded with await");//
-    let file = bucket.file(after_encode);
-    return file.getSignedUrl({
-      action: 'read',
-      expires: '03-09-2500'
-    });
-  })
-    .then(urls => {
-      var url = urls[0];
-      console.log(`Audio url = ${url}`);
-      return url;
-    })
-    .catch(err => {
-      console.log(`Unable to upload audio ${err.message}`);
-    });
+    if (contentType.startsWith('audio/')) {
 
-});*/
+      if (!fileName.endsWith('.mp3')) {
 
-// CE CODE FONCTIONNE PARFAITEMENT
+        const bucket = gcs.bucket(fileBucket);
+        const tempFilePath = path.join(os.tmpdir(), fileName);
+
+        const targetTempFileName = fileName.replace(/\.[^/.]+$/, '') + object.metadata.senderId + '.mp3';
+        const targetTempFilePath = path.join(os.tmpdir(), targetTempFileName);
+        const targetStorageFilePath = path.join(path.dirname(filePath), targetTempFileName);
+
+        await bucket.file(filePath).download({ destination: tempFilePath });
+        console.log('Audio downloaded locally to', tempFilePath);
+
+        const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+        const ffmpeg = require('fluent-ffmpeg');
+        let command = ffmpeg(tempFilePath)
+          .setFfmpegPath(ffmpegPath)
+          .toFormat('mp3')
+
+          .save(targetTempFilePath);
+
+
+        await promisifyCommand(command);
+        console.log('Output audio created at', targetTempFilePath);
+
+
+
+        console.log("delete wave file");
+        await bucket.file(filePath).delete();
+        // Uploading the audio.
+        await bucket.upload(targetTempFilePath, { destination: targetStorageFilePath }).then(stuff => {
+          console.log('Audio saved successfully.');
+          //bucket.upload(targetTempFilePath);
+          let file = bucket.file(targetStorageFilePath);
+          return file.getSignedUrl({
+            action: 'read',
+            expires: '03-09-2500'
+          });
+        }).then(urls => {
+          var url = urls[0];
+          console.log(`Audio url = ${url}`);
+          // UPDATE REALTIME DATABASE ICI ET NE PAS OUBLIER DE SET LE SEEN
+          let dataMessage = {
+            "senderId": object.metadata.senderId, //sender firebase token
+            "memberId": object.metadata.memberId, //reciever firebase token
+            "memberLastOs": object.metadata.memberLastOs,
+            "memberRegistrationId": object.metadata.memberRegistrationId,
+            "memberprofilePic": object.metadata.memberprofilePic,
+            "senderPrivateId": object.metadata.senderPrivateId,
+            "senderFullName": object.metadata.senderFullName,
+            "chatId": object.metadata.chatId,
+            "message": object.metadata.message,
+            "Environnement": object.metadata.Environnement,
+            "seen_by": {
+              [object.metadata.senderId]: true
+            },
+            "image": object.metadata.image,
+            "audio": url,
+            "time": Date.now()
+          };
+          return AddMessageToFirebase(dataMessage);
+        }).catch(err => {
+          console.log(`Unable to upload audio ${err.message}`);
+        });
+
+        fs.unlinkSync(tempFilePath);
+        fs.unlinkSync(targetTempFilePath);
+        return null;
+      }
+      return null;
+    }
+    return null;
+  }
+  return null;
+
+});
+
+
+
+
+
+
+
+
+/*--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV-
+  --DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV-
+  --DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV--DEV- */
 
 exports.DevEncodeMp3 = functions.storage.object().onFinalize(async (object) => {
   const fileBucket = object.bucket;
@@ -153,15 +212,12 @@ exports.DevEncodeMp3 = functions.storage.object().onFinalize(async (object) => {
 
 
 function AddMessageToFirebase(dataMessage) {
-  if (dataMessage.Environnement === "dev") {
-    let ref = admin.database().ref(dataMessage.Environnement + '/messages/' + dataMessage.chatId);
-    ref.push().set(dataMessage).then(() => {
-      return GetMessageId(dataMessage);
-    }).catch(err => {
-      console.log(`Unable to Add message to Firebase ${err.message}`);
-    });
-  }
-
+  let ref = admin.database().ref(dataMessage.Environnement + '/messages/' + dataMessage.chatId);
+  ref.push().set(dataMessage).then(() => {
+    return GetMessageId(dataMessage);
+  }).catch(err => {
+    console.log(`Unable to Add message to Firebase ${err.message}`);
+  });
 }
 
 function GetMessageId(dataMessage) {
@@ -238,3 +294,48 @@ function UpdateLastMessage(dataMessage) {
     console.log(`Unable get message id from Firebase ${err.message}`);
   });
 }
+
+
+/*---------------------------------------TEST ENCODE BEFORE UPLOAD------------------------------------*/
+/**
+ * When an audio is uploaded in the Storage bucket We generate a mono channel audio automatically using
+ * node-fluent-ffmpeg.
+ */
+
+/*exports.UploadVoiceNote = functions.https.onCall(async (data, context) => {
+
+  const uuidv1 = require('uuid/v1');
+  console.log('Saving audio to GCS.');
+  var bucket = admin.storage().bucket('flow-85249.appspot.com');
+  var uuid = uuidv1();
+  let before_encode = `${data.time}.wav`;
+  let after_encode = `${data.time}${data.senderId}.mp3`;
+  const tempFilePath = path.join(os.tmpdir(), before_encode);
+  const targetTempFilePath = path.join(os.tmpdir(), after_encode);
+  const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+  const ffmpeg = require('fluent-ffmpeg');
+
+  var command = ffmpeg(data.sound)
+    .setFfmpegPath(ffmpegPath)
+    .toFormat('mp3')
+    .save(targetTempFilePath);
+
+  await promisifyCommand(command);
+  await bucket.upload(targetTempFilePath, { destination: after_encode }).then(stuff => {
+    console.log("MP3 succesfully uploaded with await");//
+    let file = bucket.file(after_encode);
+    return file.getSignedUrl({
+      action: 'read',
+      expires: '03-09-2500'
+    });
+  })
+    .then(urls => {
+      var url = urls[0];
+      console.log(`Audio url = ${url}`);
+      return url;
+    })
+    .catch(err => {
+      console.log(`Unable to upload audio ${err.message}`);
+    });
+
+});*/
